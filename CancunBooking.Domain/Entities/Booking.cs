38058f5d052404483ffde014c1b53caa3ee30327
Arthur de Oliveira Batista <arthurdeol@gmail.com>
2022-03-24 00:06:53 -0300

    feat : creating booking Entity

---
+++ CancunBooking.Domain/Entities/Booking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CancunBooking.Domain.Entities
+{
+    public class Booking
+    {
+        public Booking(DateTime checkIn, DateTime checkOut)
+        {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+            Active = true;
+        }
+
+        public int Id { get; set; }
+        public DateTime CheckIn { get; private set; }
+        public DateTime CheckOut { get; private set; }
+        public bool Active { get; private set; }
+
+        public void CancelBooking() => Active = false;
+        public void ModifyBooking(DateTime checkIn, DateTime checkOut)
+        {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+    }
+}
